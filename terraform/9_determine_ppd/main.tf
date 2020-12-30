48bc172b60e1b9f3a09ac3ebc332cccf6bd954d9
raph84 <raph.berube@gmail.com>
2020-12-30 17:45:56 -0500

    timeout

--- terraform/9_determine_ppd/main.tf
+++ terraform/9_determine_ppd/main.tf
@@ -99,7 +99,7 @@ resource "google_cloud_run_service" "default" {
     spec {
       container_concurrency = 1
       service_account_name  = "thermostat-agent@thermostat-292016.iam.gserviceaccount.com"
-      timeout_seconds       = 75
+      timeout_seconds       = 180
 
       containers {
         args    = []
